bf6cadad75af280d4d08e4df5134d9b99244d0cd
Leon Heuer <leon.heuer@outlook.de>
2023-12-26 18:05:02 +0100

    Implement interface for tasks

--- src/simple_task_manager/core.clj
+++ src/simple_task_manager/core.clj
@@ -1,9 +1,13 @@
-(ns simple-task-manager.core)
+(ns simple-task-manager.core
+  (:require [simple-task-manager.tasks :as tasks]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "geht das" "Hello, World!"))
-
-(foo 2)
-(foo 3)
+(defn -main []
+  (let [task-1 (tasks/create-task "Test" 2)
+        task-2 (tasks/create-task "Test2" 1)
+        task-3 (tasks/create-task "Test3" 2)
+        all-tasks (-> {}
+                      (tasks/add-task task-1)
+                      (tasks/add-task task-2)
+                      (tasks/add-task task-3)
+                      (tasks/delete-task 1))]
+    (tasks/get-tasks all-tasks)))
---
+++ src/simple_task_manager/tasks.clj
@@ -0,0 +1,28 @@
+(ns simple-task-manager.tasks)
+
+(defrecord Task [description priority])
+
+(defn create-task [description priority]
+  (if (contains? (set [1 2 3 4]) priority)
+    (->Task description priority)
+    (throw (Throwable. "Priority must be within 1 and 4"))))
+
+(defn new-task-id [tasks]
+  (if (empty? tasks)
+    0
+    (+ 1 (apply max (keys tasks)))))
+
+(defn add-task [tasks new-task]
+  (assoc tasks
+    (new-task-id tasks)
+    new-task))
+
+(defn delete-task [tasks id]
+  (dissoc tasks id))
+
+(defn prettify-task [[id task]]
+  (str "(" id ") Description: " (:description task) ", P" (:priority task)))
+
+(defn get-tasks [tasks]
+  (doseq [task tasks]
+    (println (prettify-task task))))
