7e00093b4a446031b8b6e6603362c8640e68deca
Leon Heuer <leon.heuer@outlook.de>
2023-12-26 16:03:43 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to simple-task-manager
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/simple_task_manager/core.clj
@@ -0,0 +1,9 @@
+(ns simple-task-manager.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "geht das" "Hello, World!"))
+
+(foo 2)
+(foo 3)
